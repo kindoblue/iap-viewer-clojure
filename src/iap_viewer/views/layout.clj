d511303806e18c41bdb5d86f3cc20414f1be8f51
Stefano S <kindoblue@gmail.com>
2014-11-23 14:50:37 +0100

    added jquery.min.map

--- src/iap_viewer/views/layout.clj
+++ src/iap_viewer/views/layout.clj
@@ -1,9 +1,10 @@
 (ns iap-viewer.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
+  (:require [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn common [& body]
   (html5
     [:head
-     [:title "Welcome to iap-viewer"]
-     (include-css "/css/screen.css")]
+     [:title "Apple In-App Purchases dumper/verifier"]
+     (include-css "css/bootstrap-sortable.css" "css/bootstrap.min.css")
+     (include-js "js/bootstrap-sortable.js" "js/bootstrap.min.js" "js/jquery.min.js" "js/moment.min.js")]
     [:body body]))
