d2ec260d82aaa700d3bde68cc7329670d691ca1f
Stefano S <kindoblue@gmail.com>
2014-11-24 22:20:39 +0100

    add bouncy castle deps

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [org.bouncycastle/bcprov-jdk15on "1.50"]
+                 [org.bouncycastle/bcmail-jdk15on "1.50"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.50"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler iap-viewer.handler/app
          :init iap-viewer.handler/init
--- src/iap_viewer/routes/home.clj
+++ src/iap_viewer/routes/home.clj
@@ -1,6 +1,12 @@
 (ns iap-viewer.routes.home
   (:require [compojure.core :refer :all]
-            [iap-viewer.views.layout :as layout]))
+            [iap-viewer.views.layout :as layout])
+  (:import (org.bouncycastle.asn1 ASN1InputStream ASN1Primitive)
+           (org.bouncycastle.asn1.cms ContentInfo SignedData)
+           (org.bouncycastle.cms CMSSignedData)
+           (org.bouncycastle.jce.provider BouncyCastleProvider)
+           (java.security.cert CertificateFactory)
+           (java.security Security)))
 
 (defn home []
   (layout/common [:h1 "Hello World!"]))
