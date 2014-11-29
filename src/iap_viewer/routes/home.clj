7258abec2fe2797ceab93374d3a599cea813fe25
Stefano S <kindoblue@gmail.com>
2014-11-29 22:03:44 +0100

    reshuffling

--- src/iap_viewer/routes/home.clj
+++ src/iap_viewer/routes/home.clj
@@ -14,82 +14,52 @@
 (def receipt-url (clojure.java.io/resource "1000000101882225.cer"))
 
 
-(defn get-content-info
-  "Get the content info object representing the Apple receipt"
-  [receipt-url]
-  (-> (.openStream receipt-url)
-      ASN1InputStream.
-      .readObject
-      ContentInfo/getInstance))
-
-(defn get-signed-data
-  "Get the all the info inside the Apple receipt, returning as enumeration-seq"
-  [receipt-url]
-  (->(get-content-info receipt-url)
-     CMSSignedData.
-     .getSignedContent
-     .getContent
-     ASN1Primitive/fromByteArray
-     .getObjects
-     enumeration-seq))
-
-(defn get-receipt-fields [x]
-  (-> (.getObjects x)
-      enumeration-seq))
-
 ;; the input is a raw entry, in the form of a DLSequence #<DLSequence [a, b, #<content>]>
 ;; in case of purchase --> a = 17 and b = 1
-(defn is-purchase
+(defn- is-purchase
   "Return true if the input is a purchase"
   [^org.bouncycastle.asn1.DLSequence x]
-  (let [fields (get-receipt-fields x)
-        a (first fields)
-        b (second fields)]
+  (let [a (.getObjectAt x 0)
+        b (.getObjectAt x 1)]
     (and (= 17 (.. a getValue intValue)) (= 1 (.. b getValue intValue)))))
 
 
-;; return a list of org.bouncycastle.asn1.DLSequence objects
-;; representing the purchases
-(defn get-purchases-raw [records]
-  (filter is-purchase records))
-
-
-(defn parse-quantity [^org.bouncycastle.asn1.DEROctetString input]
+(defn- parse-quantity [^org.bouncycastle.asn1.DEROctetString input]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input))
         b (.. a getValue intValue)]
     {:quantity b}))
 
-(defn parse-product-id [^org.bouncycastle.asn1.DEROctetString input]
+(defn- parse-product-id [^org.bouncycastle.asn1.DEROctetString input]
    (let [a (ASN1Primitive/fromByteArray (.getOctets input))
          b (.getString a)]
      {:product-id b}))
 
-(defn parse-transaction-id [^org.bouncycastle.asn1.DEROctetString input]
+(defn- parse-transaction-id [^org.bouncycastle.asn1.DEROctetString input]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input))
         b (.getString a)]
     {:transaction-id b}))
 
-(defn parse-original-transaction-id [^org.bouncycastle.asn1.DEROctetString input]
+(defn- parse-original-transaction-id [^org.bouncycastle.asn1.DEROctetString input]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input))
         b (.getString a)]
     {:org-transaction-id b}))
 
-(defn parse-purchase-date [^org.bouncycastle.asn1.DEROctetString input-date]
+(defn- parse-purchase-date [^org.bouncycastle.asn1.DEROctetString input-date]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
         b (.getString a)]
     {:purchase-date b}))
 
-(defn parse-original-purchase-date [^org.bouncycastle.asn1.DEROctetString input-date]
+(defn- parse-original-purchase-date [^org.bouncycastle.asn1.DEROctetString input-date]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
         b (.getString a)]
     {:org-purchase-date b}))
 
-(defn parse-subscription-exp-date [^org.bouncycastle.asn1.DEROctetString input-date]
+(defn- parse-subscription-exp-date [^org.bouncycastle.asn1.DEROctetString input-date]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
         b (.getString a)]
     {:subscription-exp-date b}))
 
-(defn parse-cancellation-date [^org.bouncycastle.asn1.DEROctetString input-date]
+(defn- parse-cancellation-date [^org.bouncycastle.asn1.DEROctetString input-date]
   (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
         b (.getString a)]
     {:cancellation-date b}))
@@ -97,7 +67,7 @@
 
 ;; see https://developer.apple.com/library/ios/releasenotes/General/ValidateAppStoreReceipt/Chapters/ReceiptFields.html
 ;;
-(defn parse-purchase-field [^org.bouncycastle.asn1.DLSequence x]
+(defn- parse-purchase-field [^org.bouncycastle.asn1.DLSequence x]
   (let [^org.bouncycastle.asn1.ASN1Integer field-type-obj (.getObjectAt x 0)
         field-type (.. field-type-obj getValue intValue)
         field-value (.getObjectAt x 2)]
@@ -109,19 +79,44 @@
      (= field-type 1704) (parse-purchase-date field-value)
      (= field-type 1706) (parse-original-purchase-date field-value)
      (= field-type 1708) (parse-subscription-exp-date field-value)
-     (= field-type 1712) (parse-cancellation-date field-value)
-     :else nil)))
+     (= field-type 1712) (parse-cancellation-date field-value))))
 
 
 ;; it gets a purchase as #<DLSequence [17, 1, #<DEROctetString>]>, i.e. a sequence
 ;; of three objects. The third one is a byte sequence to be interpreted as a DLSet,
 ;; the set of fields of the purchase record
-(defn parse-purchase [^org.bouncycastle.asn1.DLSequence raw-purchase]
+(defn- parse-purchase [^org.bouncycastle.asn1.DLSequence raw-purchase]
   (let [purchase-as-dlset (ASN1Primitive/fromByteArray (.getOctets (.getObjectAt raw-purchase 2)))
-        field-seq (enumeration-seq (.getObjects purchase-as-dlset))]
-    (reduce conj {} (map parse-purchase-field field-seq))))
+        field-sequence (enumeration-seq (.getObjects purchase-as-dlset))]
+    (reduce conj {} (map parse-purchase-field field-sequence))))
 
-(map parse-purchase (get-purchases-raw (get-signed-data receipt-url)))
+
+;; return a list of org.bouncycastle.asn1.DLSequence objects
+;; representing the purchases
+(defn- get-purchases [records]
+  (filter is-purchase records))
+
+(defn- get-content-info
+  "Get the content info object representing the Apple receipt"
+  [receipt-url]
+  (-> (.openStream receipt-url)
+      ASN1InputStream.
+      .readObject
+      ContentInfo/getInstance))
+
+
+(defn- get-signed-data
+  "Get the all the info inside the Apple receipt, returning as enumeration-seq"
+  [receipt-url]
+  (->(get-content-info receipt-url)
+     CMSSignedData.
+     .getSignedContent
+     .getContent
+     ASN1Primitive/fromByteArray
+     .getObjects
+     enumeration-seq))
+
+(map parse-purchase (get-purchases (get-signed-data receipt-url)))
 
 ;; for example
 (get-signed-data receipt-url)
