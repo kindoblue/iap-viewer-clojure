7f417fbdb7d35499e1964941db6709eab399f1a0
Stefano S <kindoblue@gmail.com>
2014-12-02 21:53:13 +0100

    refactored and completed the basic functionality. Added also the test

--- src/iap_viewer/routes/home.clj
+++ src/iap_viewer/routes/home.clj
@@ -1,134 +1,37 @@
 (ns iap-viewer.routes.home
   (:require [compojure.core :refer :all]
             [iap-viewer.views.layout :as layout]
-            (ring.middleware [multipart-params :as mp]))
-  (:import (org.bouncycastle.asn1 ASN1InputStream ASN1Primitive)
-           (org.bouncycastle.asn1.cms ContentInfo SignedData)
-           (org.bouncycastle.cms CMSSignedData)
-           (org.bouncycastle.jce.provider BouncyCastleProvider)
-           (java.security.cert CertificateFactory)
-           (java.security Security)))
+            [iap-viewer.core :as core]
+            [clojure.java.io :as io]
+            (ring.middleware [multipart-params :as mp])))
 
 
-;; test receipt
-(def receipt-url (clojure.java.io/resource "1000000101882225.cer"))
 
-
-;; the input is a raw entry, in the form of a DLSequence #<DLSequence [a, b, #<content>]>
-;; in case of purchase --> a = 17 and b = 1
-(defn- is-purchase
-  "Return true if the input is a purchase"
-  [^org.bouncycastle.asn1.DLSequence x]
-  (let [a (.getObjectAt x 0)
-        b (.getObjectAt x 1)]
-    (and (= 17 (.. a getValue intValue)) (= 1 (.. b getValue intValue)))))
-
-
-(defn- parse-quantity [^org.bouncycastle.asn1.DEROctetString input]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input))
-        b (.. a getValue intValue)]
-    {:quantity b}))
-
-(defn- parse-product-id [^org.bouncycastle.asn1.DEROctetString input]
-   (let [a (ASN1Primitive/fromByteArray (.getOctets input))
-         b (.getString a)]
-     {:product-id b}))
-
-(defn- parse-transaction-id [^org.bouncycastle.asn1.DEROctetString input]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input))
-        b (.getString a)]
-    {:transaction-id b}))
-
-(defn- parse-original-transaction-id [^org.bouncycastle.asn1.DEROctetString input]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input))
-        b (.getString a)]
-    {:org-transaction-id b}))
-
-(defn- parse-purchase-date [^org.bouncycastle.asn1.DEROctetString input-date]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
-        b (.getString a)]
-    {:purchase-date b}))
-
-(defn- parse-original-purchase-date [^org.bouncycastle.asn1.DEROctetString input-date]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
-        b (.getString a)]
-    {:org-purchase-date b}))
-
-(defn- parse-subscription-exp-date [^org.bouncycastle.asn1.DEROctetString input-date]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
-        b (.getString a)]
-    {:subscription-exp-date b}))
-
-(defn- parse-cancellation-date [^org.bouncycastle.asn1.DEROctetString input-date]
-  (let [a (ASN1Primitive/fromByteArray (.getOctets input-date))
-        b (.getString a)]
-    {:cancellation-date b}))
-
-
-;; see https://developer.apple.com/library/ios/releasenotes/General/ValidateAppStoreReceipt/Chapters/ReceiptFields.html
-;;
-(defn- parse-purchase-field [^org.bouncycastle.asn1.DLSequence x]
-  (let [^org.bouncycastle.asn1.ASN1Integer field-type-obj (.getObjectAt x 0)
-        field-type (.. field-type-obj getValue intValue)
-        field-value (.getObjectAt x 2)]
-    (cond
-     (= field-type 1701) (parse-quantity field-value)
-     (= field-type 1702) (parse-product-id field-value)
-     (= field-type 1703) (parse-transaction-id field-value)
-     (= field-type 1705) (parse-original-transaction-id field-value)
-     (= field-type 1704) (parse-purchase-date field-value)
-     (= field-type 1706) (parse-original-purchase-date field-value)
-     (= field-type 1708) (parse-subscription-exp-date field-value)
-     (= field-type 1712) (parse-cancellation-date field-value))))
-
-
-;; it gets a purchase as #<DLSequence [17, 1, #<DEROctetString>]>, i.e. a sequence
-;; of three objects. The third one is a byte sequence to be interpreted as a DLSet,
-;; the set of fields of the purchase record
-(defn- parse-purchase [^org.bouncycastle.asn1.DLSequence raw-purchase]
-  (let [purchase-as-dlset (ASN1Primitive/fromByteArray (.getOctets (.getObjectAt raw-purchase 2)))
-        field-sequence (enumeration-seq (.getObjects purchase-as-dlset))]
-    (reduce conj {} (map parse-purchase-field field-sequence))))
-
-
-;; return a list of org.bouncycastle.asn1.DLSequence objects
-;; representing the purchases
-(defn- get-purchases [records]
-  (filter is-purchase records))
-
-(defn- get-content-info
-  "Get the content info object representing the Apple receipt"
-  [receipt-url]
-  (-> (.openStream receipt-url)
-      ASN1InputStream.
-      .readObject
-      ContentInfo/getInstance))
-
-
-(defn- get-signed-data
-  "Get the all the info inside the Apple receipt, returning as enumeration-seq"
-  [receipt-url]
-  (->(get-content-info receipt-url)
-     CMSSignedData.
-     .getSignedContent
-     .getContent
-     ASN1Primitive/fromByteArray
-     .getObjects
-     enumeration-seq))
-
-
-;; for testing purposes only
-(def test-results (map parse-purchase (get-purchases (get-signed-data receipt-url))))
-
-;; {:size 62063, :tempfile #<File /var/folders/9_/0vs5vx611s74xpg0kymc0q3r0000gn/T/ring-multipart-2430006034048162240.tmp>, :content-type application/pdf, :filename short_curriculum.pdf}
-(defn- parse-file [file]
-  (println file)
-  (home (map parse-purchase (get-purchases (get-signed-data (file :tempfile))))))
-
+;; home page definition
+;; in input the list of purchases
 (defn home [purchases]
   (layout/common
    (layout/upload-display-page purchases)))
 
+;;
+;; parse the receipt file and render the
+;; home page
+;;
+;; in input you have a map containing the
+;; tempfile key, i.e. the uploaded receipt
+;;
+;; {:size 62063,
+;;  :tempfile #<File /var/folders/9_/0vs5vx611s74xpg0kymc0q3r0000gn/T/ring-multipart-2430006034048162240.tmp>,
+;;  :content-type application/pdf,
+;;  :filename blablala.der}
+;;
+(defn- parse-file [file-map]
+  (let [receipt-url (io/as-url (file-map :tempfile))]
+    (home (core/get-purchases-from-url receipt-url))))
+
+;;
+;; routes definitions
+;;
 (defroutes home-routes
   (GET "/" [] (home '()))
   (mp/wrap-multipart-params
--- test/iap_viewer/test/handler.clj
+++ test/iap_viewer/test/handler.clj
@@ -1,14 +1,33 @@
 (ns iap-viewer.test.handler
   (:use clojure.test
         ring.mock.request
-        iap-viewer.handler))
+        iap-viewer.handler
+        iap-viewer.core))
+
+(def expected-home-page "<!DOCTYPE html>\n<html><head><title>Apple In-App Purchases dumper/verifier</title><link href=\"css/bootstrap-sortable.css\" rel=\"stylesheet\" type=\"text/css\"><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"><script src=\"js/bootstrap-sortable.js\" type=\"text/javascript\"></script><script src=\"js/bootstrap.min.js\" type=\"text/javascript\"></script><script src=\"js/jquery.min.js\" type=\"text/javascript\"></script><script src=\"js/moment.min.js\" type=\"text/javascript\"></script></head><body><div class=\"container\"><div class=\"page-header\"><h1>Apple In-App Purchases dumper</h1></div><p class=\"lead\">The receipt you receive from Apple is DER encoded PKCS#7 signed message. You can upload a receipt and see the content here. For the moment the receipt is not validated.</p><div class=\"row\"><div class=\"col-md-6\"><form action=\"/verify\" enctype=\"multipart/form-data\" method=\"post\"><div class=\"form-group\"><input id=\"der-input-file\" name=\"upfile\" size=\"20000\" type=\"file\" /><div class=\"help-block\">Select an Apple receipt (DER format)</div></div><input class=\"btn btn-default\" name=\"submit\" type=\"submit\" value=\"Upload\" /></form></div></div></div></body></html>")
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) expected-home-page))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+
+;; test receipt
+(deftest test-parse-receipt
+  (testing "parsing the receipt"
+    (let [receipt-url  (clojure.java.io/resource "1000000101882225.cer")
+          purchases (get-purchases-from-url receipt-url)]
+      (let [{:keys [org-purchase-date purchase-date product-id org-transaction-id transaction-id quantity cancellation-date subscription-exp-date]} (first purchases)]
+        (is (= org-purchase-date "2014-02-18T16:18:09Z"))
+        (is (= purchase-date "2014-02-19T13:26:54Z"))
+        (is (= product-id "1_month_subscription"))
+        (is (= org-transaction-id "1000000101874971"))
+        (is (= transaction-id "1000000101874971"))
+        (is (= quantity 1))
+        (is (= cancellation-date ""))
+        (is (= subscription-exp-date ""))))))
