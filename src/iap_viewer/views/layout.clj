e014efa616f6b2da508edb49cf085cf618c27d93
Stefano S <kindoblue@gmail.com>
2014-12-06 00:30:02 +0100

    jquery has to be loaded first, otherwise bootstrap and bootstrap-sortable dont work ;-)

--- src/iap_viewer/views/layout.clj
+++ src/iap_viewer/views/layout.clj
@@ -7,7 +7,7 @@
     [:head
      [:title "Apple In-App Purchases dumper/verifier"]
      (include-css "css/bootstrap-sortable.css" "css/bootstrap.min.css")
-     (include-js "js/bootstrap-sortable.js" "js/bootstrap.min.js" "js/jquery.min.js" "js/moment.min.js")]
+     (include-js "js/jquery.min.js" "js/bootstrap-sortable.js" "js/bootstrap.min.js" "js/moment.min.js")]
     [:body body]))
 
 ;; helper function, to add the upload form
